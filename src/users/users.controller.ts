d07de3b842ecfeadac5f7644a13fcbd1130caaeb
umeshmore45 <umesh.more@contentstack.com>
2022-09-03 13:25:00 +0530

    added create user route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.gto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+
+  constructor(private usersService: UsersService) { }
+
+  @Post("/signup")
+  createUser(@Body() body: CreateUserDto) {
+    console.log("🚀 ~ file: users.controller.ts ~ line 8 ~ UsersController ~ createUser ~ body", body)
+    this.usersService.create(body.email, body.password)
+  }
+}
